874e2ec258c1577035873aa097c63cbe47b510d5
Kostyan89 <kostasamin@gmail.com>
2022-04-19 20:23:07 +0300

    add app core

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -13,21 +13,21 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 from os import environ
-from environ import Env
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 from dotenv import load_dotenv
-env = environ.Env(
-    DEBUG=(bool, False)
-)
+from environ import Env
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+env = Env()
+env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", False)
