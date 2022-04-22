a35d73889903a6401d12665c6a4775d2531d0c3b
Kostyan89 <kostasamin@gmail.com>
2022-04-22 19:53:21 +0300

    some changes

--- todolist/core/admin.py
+++ todolist/core/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 from core.models import User
 
-admin.site.register(User)
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    exclude = ("password",)
+    readonly_fields = ("last_login", "date_joined")
+    list_display = ("username", "email", "first_name", "last_name")
+    list_filter = ("is_staff", "is_active", "is_superuser")
+    search_fields = ("email", "first_name", "last_name")
--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -27,11 +27,11 @@ env.read_env()
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = 'asdasdasdasdasdasd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool("DEBUG", False)
-
+# DEBUG = env.bool("DEBUG", False)
+DEBUG = True
 ALLOWED_HOSTS = ["*"]
 
 
@@ -87,9 +87,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': os.environ['POSTGRES_PASSWORD'],
+        'PASSWORD': 'postgres',
         'PORT': '5432',
-        'HOST': os.environ.get('DB_HOST', '127.0.0.1'),
+        'HOST': 'localhost'
     }
 }
 
