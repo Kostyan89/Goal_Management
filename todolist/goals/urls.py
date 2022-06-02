ffc8950261788fd0cffc62fb8ec83dff14bd2c73
Kostyan89 <kostasamin@gmail.com>
2022-06-02 10:41:19 +0300

    some commit

--- todolist/goals/urls.py
+++ todolist/goals/urls.py
@@ -15,10 +15,10 @@ urlpatterns = [
     path("goal_comment/list", comments.CommentListView.as_view()),
     path("goal_comment/<pk>", comments.CommentView.as_view()),
 
-    path('board/', include((
-        [
-            path('create', boards.BoardCreateView.as_view(), name='create'),
-            path('<pk>', boards.BoardView.as_view(), name='main'),
-            path('list', boards.BoardListView.as_view(), name='list'),
-        ], 'board'), namespace='board'))
-]
+
+
+    path('board/create', boards.BoardCreateView.as_view()),
+    path('board/<pk>', boards.BoardView.as_view()),
+    path('board/list', boards.BoardListView.as_view())
+   ]
+
