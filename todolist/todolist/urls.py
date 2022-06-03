bde188e790da07f5e8fd20a72c4ecff31e483c65
Kostyan89 <kostasamin@gmail.com>
2022-06-03 20:06:09 +0300

    some commit

--- todolist/todolist/urls.py
+++ todolist/todolist/urls.py
@@ -36,6 +36,5 @@ urlpatterns = [
     path('oauth/', include('social_django.urls', namespace='social')),
     path('core/', include("core.urls")),
     path("goals/", include("goals.urls")),
-    path("boards/", include("boards.urls")),
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
