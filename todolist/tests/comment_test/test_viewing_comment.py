d0b89b0df1d776568847e23420825d749971b391
Kostyan89 <kostasamin@gmail.com>
2022-06-05 21:31:58 +0300

    bot is working

--- todolist/tests/comment_test/test_viewing_comment.py
+++ todolist/tests/comment_test/test_viewing_comment.py
@@ -3,16 +3,16 @@ import pytest
 from core.models import User
 from goals.serializers import CommentSerializer
 
-#
-# @pytest.mark.django_db
-# def test_viewing_comment(client, logged_in_user1, comment):
-#     comment = comment
-#     expected_response = CommentSerializer(comment).data
-#
-#     response = client.get(f"/goals/goal_comment/{comment.id}")
-#
-#     assert response.status_code == 200
-#     assert response.json() == expected_response
+
+@pytest.mark.django_db
+def test_viewing_comment(client, logged_in_user1, comment):
+    comment = comment
+    expected_response = CommentSerializer(comment).data
+
+    response = client.get(f"/goals/goal_comment/{comment.id}")
+
+    assert response.status_code == 200
+    assert response.json() == expected_response
 
 
 @pytest.mark.django_db
